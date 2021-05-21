07e8e55d8d43bef63aa81726c63ffcf49a7ec4f5
Theo Dedeken <theo.dedeken@telenet.be>
2021-05-21 22:53:16 +0200

    finish auto update script

--- bazel/build/oregano/oregano_autoupdate.py
+++ bazel/build/oregano/oregano_autoupdate.py
@@ -3,24 +3,20 @@ import argparse
 import subprocess
 
 
-def autoupdate_file(build_file, target_name):
+def autoupdate_file(target_name):
     deps = get_deps()
-    write_oregano_rule(target_name, deps, True)
+    depstr = " ".join(deps)
 
-
-def write_oregano_rule(name, deps, autoupdate):
-    print(f"oregano(")
-    print(f"    name = {name},")
-    print(f"    autoupdate = {autoupdate},")
-    print(f"    deps = [")
-    for dep in sorted(deps):
-        print(" " * 8 + f'"{dep}",')
-    print(f"    ],")
-    print(f")")
+    out = subprocess.check_output(
+        f"buildozer 'set deps {depstr}' {target_name}",
+        cwd=os.environ["BUILD_WORKSPACE_DIRECTORY"],
+        text=True,
+        shell=True,
+    )
+    print(out)
 
 
 def get_deps():
-    print(os.environ["BUILD_WORKSPACE_DIRECTORY"])
     out = subprocess.check_output(
         ["bazel", "query", "attr(tags, 'RGNO', //...)"],
         cwd=os.environ["BUILD_WORKSPACE_DIRECTORY"],
@@ -34,14 +30,9 @@ if __name__ == "__main__":
         description="Autoupdate build files with all dependencies to rgno files"
     )
     parser.add_argument(
-        "build_file",
-        type=str,
-        help="The build file to edit",
-    )
-    parser.add_argument(
         "target_name",
         type=str,
         help="The target name to edit in the build file",
     )
     args = parser.parse_args()
-    autoupdate_file(args.build_file, args.target_name)
+    autoupdate_file(args.target_name)
