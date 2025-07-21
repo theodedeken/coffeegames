7cb292dc1c36f80434a95045e4ce988582a2eebc
Theo Dedeken <theo.dedeken@telenet.be>
2025-07-21 21:00:03 +0200

    Flake setup and bevy experiment

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src" # for rust-analyzer
+            "rust-analyzer"
+          ];
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell rec {
+            nativeBuildInputs = [
+              pkg-config
+            ];
+            buildInputs = [
+              udev
+              alsa-lib
+              vulkan-loader
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr # To use the x11 feature
+              libxkbcommon
+              wayland # To use the wayland feature
+              rust
+              cargo
+            ];
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          };
+        }
+    );
+}
